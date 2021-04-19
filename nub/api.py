39a283fa1b4a7c77313023973805d171f1823e8f
ahmadRagheb <ahmedragheb75@gmail.com>
2021-04-19 15:15:10 +0300

    Update api.py

--- nub/api.py
+++ nub/api.py
@@ -4,8 +4,8 @@ from . import __version__ as app_version
 import frappe
 
 def check_asset_barcode(doc, method):
-    # if doc.barcode_serial_number and doc.barcode_serial_number != '':
-    #     doc.db_set('is_available', 1)
+    if doc.barcode_serial_number and doc.barcode_serial_number != '':
+        doc.db_set('is_available', 1)
     doc.barcode_serial_number = doc.barcode
     frappe.db.set_value(doc.doctype, doc.name, "barcode_serial_number", doc.barcode)
     frappe.db.commit()
